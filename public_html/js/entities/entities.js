93067edd5a6a877570f5c837b8ec50e0ae53f14e
Esme Grundy <eg2485@gmail.com>
2014-12-02 11:28:00 -0800

    Added Mushroom/Changed Level 3

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -25,26 +25,26 @@ game.PlayerEntity = me.Entity.extend({
             this.flipX(false);
             this.body.vel.x += this.body.accel.x * me.timer.tick;
         }
-        else if(me.input.isKeyPressed("left")){
+        else if (me.input.isKeyPressed("left")) {
             this.flipX(true);
             this.body.vel.x -= this.body.accel.x * me.timer.tick;
         }
         else {
             this.body.vel.x = 0;
         }
-        
-        if(me.input.isKeyPressed("jump")){
-            if(!this.body.jumping && !this.body.falling){
+
+        if (me.input.isKeyPressed("jump")) {
+            if (!this.body.jumping && !this.body.falling) {
                 this.body.vel.y = -this.body.maxVel.y * me.timer.tick;
                 this.body.jumping = true;
             }
         }
-        
+
         this.body.update(delta);
         me.collision.check(this, true, this.collideHandler.bind(this), true);
-        
+
         if (this.body.vel.x !== 0) {
-            if (!this.renderable.isCurrentAnimation("smallWalk")){
+            if (!this.renderable.isCurrentAnimation("smallWalk")) {
                 this.renderable.setCurrentAnimation("smallWalk");
                 this.renderable.setAnimationFrame();
             }
@@ -53,35 +53,43 @@ game.PlayerEntity = me.Entity.extend({
             this.renderable.setCurrentAnimation("idle");
         }
 
-        
+
         this._super(me.Entity, "update", [delta]);
         return true;
     },
-    
-    collideHandler: function(response){
+    collideHandler: function(response) {
+        var ydif = this.pos.y - response.b.pos.y;
+        console.log(ydif);
         
+        if(response.b.type === 'badguy'){
+            if(ydif <= -115){
+                response.b.alive = false;
+            }
+            else{
+                me.state.change(me.state.MENU);
+            }
+        }
     }
 });
 
 game.LevelTrigger = me.Entity.extend({
-   init: function(x, y, settings){
-       this._super(me.Entity, 'init', [x, y, settings]);
-       this.body.onCollision = this.onCollision.bind(this);
-       this.level = settings.level;
-       this.xSpawn = settings.xSpawn;
-       this.ySpawn = settings.ySpawn;
-   } ,
-   
-   onCollision: function(){
-       this.body.setCollisionMask(me.collision.types.NO_OBJECT);
-       me.levelDirector.loadLevel(this.level);
-       me.state.current().resetPlayer();
-   }
+    init: function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, settings]);
+        this.body.onCollision = this.onCollision.bind(this);
+        this.level = settings.level;
+        this.xSpawn = settings.xSpawn;
+        this.ySpawn = settings.ySpawn;
+    },
+    onCollision: function() {
+        this.body.setCollisionMask(me.collision.types.NO_OBJECT);
+        me.levelDirector.loadLevel(this.level);
+        me.state.current().resetPlayer();
+    }
 });
 
 game.BadGuy = me.Entity.extend({
-    init: function(x, y, settings){
-           this._super(me.Entity, 'init', [x, y, {
+    init: function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, {
                 image: "slime",
                 spritewidth: "60",
                 spriteheight: "28",
@@ -91,50 +99,67 @@ game.BadGuy = me.Entity.extend({
                     return (new me.Rect(0, 0, 60, 28).toPolygon());
                 }
             }]);
-        
+
         this.spritewidth = 60;
         var width = settings.width;
         x = this.pos.x;
         this.startX = x;
         this.endX = x + width - this.spritewidth;
         this.pos.x = x + width - this.spritewidth;
-        
+
         this.updateBounds();
-        
+
         this.alwaysUpdate = true;
-        
+
         this.walkLeft = false;
         this.alive = true;
         this.type = "badguy";
-        
+
         this.body.setVelocity(4, 6);
     },
-    
-    update: function(){
+    update: function(delta) {
         this.body.update(delta);
         me.collision.check(this, true, this.collideHandler.bind(this), true);
-        
-        if(this.alive){
-            if(this.walkLeft && this.pos.x <= this.startX){
+
+        if (this.alive) {
+            if (this.walkLeft && this.pos.x <= this.startX) {
                 this.walkLeft = false;
             }
-            else if(!this.walkLeft && this.pos.x >= this.endX){
+            else if (!this.walkLeft && this.pos.x >= this.endX) {
                 this.walkLeft = true;
             }
+
+            this.flipX(!this.walkLeft);
+            this.body.vel.x += (this.walkLeft) ? -this.body.accel.x * me.timer.tick : this.body.accel.x * me.timer.tick;
         }
-        
-        this.flipX(!this.walkLeft);
-        this.body.vel.x += (this.walkLeft) ? -this.body.accel.x * me.timer.tick: this.body.accel.x * me.timer.tick;
-       
-        else{
+
+
+
+        else {
             me.game.world.removeChild(this);
         }
-        
+
         this._super(me.Entity, "update", [delta]);
         return true;
     },
-    
-    collideHandler: function(){
-        
+    collideHandler: function() {
+
+    }
+});
+
+game.Mushroom = me.Entity.extend({
+    init: function(x, y, settings){
+          this._super(me.Entity, 'init', [x, y, {
+                image: "mushroom",
+                spritewidth: "64",
+                spriteheight: "64",
+                width: 64,
+                height: 64,
+                getShape: function() {
+                    return (new me.Rect(0, 0, 64, 64).toPolygon());
+                }
+            }]);
+        me.collision.check(this);
+        this.type = "mushroom";
     }
 });
